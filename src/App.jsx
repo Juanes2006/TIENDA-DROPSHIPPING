d0da31089da122f6a97a07505b8bb58f39dcd4c8
Juanes2006 <juanestebanmarulanda117@gmail.com>
2025-11-19 10:07:32 -0500

    Version: Corregida 19/11/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Footer from "./components/Footer";
+import ModalContactForm from "./components/ModalContactForm";
+import LimitedOfferHeader from "./components/LimitedOfferHeader";
+import Ratings from "./components/Ratings";
+import BotonWhatsapp from "./components/boton_whatsapp";
+import Especificaciones from "./components/especificaciones.jsx";
+import ModalVideo from "./components/ModalVideo";
+
+export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    setShowVideo(true);
+  }, []);
+
+  return (
+    <div>
+      <LimitedOfferHeader onOpenModal={() => setIsModalOpen(true)} />
+
+      <ModalVideo
+        open={showVideo}
+        onClose={() => setShowVideo(false)}
+        videoSrc="/videos/stitchVideo1.mov"
+        title=" Stitch Respira Contigo"
+      />
+
+      <div className="container">
+        <Hero onOpenModal={() => setIsModalOpen(true)} />
+        <Especificaciones />
+        <Benefits />
+        <Ratings />
+      </div>
+
       <BotonWhatsapp />
-      <BotonWhatsapp />
+      <Footer />
+      <ModalContactForm isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+    </div>
+  );
+}
